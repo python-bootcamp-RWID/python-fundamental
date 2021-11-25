930c2b0849c57174832cda1c08aa5185da80aaa9
danilsyah <danilsyaharihardjo@gmail.com>
2021-11-25 14:24:08 +0700

    belajar perulangan for dan while

---
+++ perulangan-for.py
@@ -0,0 +1,14 @@
+"""
+program perulangan membaca buku
+"""
+
+jumlah_buku = 10
+print("Ibu berkata, 'baca bukumu'")
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f"jumlah buku yang sudah di baca {jumlah_buku_yang_sudah_dibaca}")
+
+for jumlah_buku_yang_sudah_dibaca in range(1, jumlah_buku+1):
+    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f"jumlah buku yang sudah di baca {jumlah_buku_yang_sudah_dibaca}")
