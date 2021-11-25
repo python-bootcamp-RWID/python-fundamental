1bd099f97d44d6fc9cf77a32f46fd5b824c48f4a
danilsyah <danilsyaharihardjo@gmail.com>
2021-11-25 15:01:10 +0700

    rename variabel in english, add condition understood read

--- perulangan-while.py
+++ perulangan-while.py
@@ -1,15 +1,24 @@
 """
 Program perulangan membaca buku dengan while
 """
-
-jumlah_buku = 10
+read_count = 0
+book_count = 10
 print("Ibu berkata, 'baca semua bukumu '")
 
-jumlah_buku_yang_sudah_dibaca = 0
-print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+understood_count = 0
+print(f"jumlah buku yang sudah dibaca dan dipahami {understood_count}")
 
-while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
-    jumlah_buku_yang_sudah_dibaca += 1
-    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+while read_count < book_count * 2:
+    read_count = read_count + 1
+    if understood_count == 9:
+        print(f"Buku ke {understood_count + 1} belum paham ")
+    else :
+        understood_count += 1
+        print(f"buku ke {understood_count} sudah dibaca dan dipahami")
 
-print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+print(f"jumlah buku yang sudah dibaca dan dipahami {understood_count}")
+if understood_count == book_count:
+    print('"Bu, semua buku sudah dibaca dan dipahami"')
+else:
+    print(f"'Bu, tidak semua buku bisa dipahami. "
+          f"Budi hanya bisa memahami {understood_count} buku")
